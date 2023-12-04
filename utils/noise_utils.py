48919b81d865a3ba92c3fe43f9dfc7e1986e21ed
Paula Domingues <pdomingues@itba.edu.ar>
2023-12-04 19:34:57 -0300

    fix: error scales

--- utils/noise_utils.py
+++ utils/noise_utils.py
@@ -9,8 +9,7 @@ def add_noise(df: pd.DataFrame, mu=0, sigma=0.1):  # mu = mean, sigma = std, los
     return noisy_df
 
 
-def add_noise_to_column(df: pd.DataFrame, column_name: str):
-    std_deviation = df[column_name].std()
-    mean = df[column_name].mean()
+def add_noise_to_column(df: pd.DataFrame, column_name: str, scale_factor: float = 0.1):
+    std_deviation = df[column_name].std() * scale_factor
 
-    return add_noise(df[column_name], mu=mean, sigma=std_deviation)
+    return add_noise(df[column_name], sigma=std_deviation)
