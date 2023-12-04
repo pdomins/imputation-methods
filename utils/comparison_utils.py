8e5d32b2bde359fd5d560fbcea5ddc835a14809a
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-12-04 04:28:17 +0000

    feat: add comparison utils

---
+++ utils/comparison_utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+def compare_imputations(df_real, df_imputed):
+    comparison_results = []
+    for column in df_real.columns:
+        mse = mean_squared_error(df_real[column], df_imputed[column])
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(df_real[column], df_imputed[column])
+        comparison_results.append({
+            'Column': column,
+            'MSE': mse,
+            'RMSE': rmse,
+            'MAE': mae
+        })
+    return pd.DataFrame(comparison_results)
