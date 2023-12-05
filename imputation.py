d588a41ede6c53328d11d4f7a982f5a3ad168870
Paula Oseroff <oseroffp@gmail.com>
2023-12-05 14:36:25 -0300

    refactor: move imputation distribution

--- imputation.py
+++ imputation.py
@@ -3,6 +3,7 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import KNNImputer, IterativeImputer
 from sklearn.linear_model import LinearRegression, BayesianRidge
+from typing import Any
 
 
 def imputed_vals(cols : list[str], df : pd.DataFrame) -> tuple[dict, dict]:
@@ -114,3 +115,45 @@ def run_comparing(labeled_df: pd.DataFrame,
 
     mse_df = pd.DataFrame(mse_df_dict)
     return mse_df, imputer_map
+
+def run(
+        labeled_df: pd.DataFrame,
+        random_missing_df: pd.DateOffset,
+        missing_vals_idxs: list,
+        missing_col_per_pos: list,
+        imputer_type: str,
+        config: dict,
+        estimator_config: dict = {}) -> tuple[pd.DataFrame, Any]:
+    
+    imputer_type_map = {
+        "kNN": lambda c, estimator_config: KNNImputer(weights="distance", **c),
+        "WkNN": lambda c, estimator_config: KNNImputer(weights="uniform", **c),
+        "MICE": lambda c, estimator_config: IterativeImputer(estimator=LinearRegression(**estimator_config), **c),
+        "MICE BR": lambda c, estimator_config: IterativeImputer(estimator=BayesianRidge(**estimator_config), **c),
+        "MICE RF": lambda c, estimator_config: IterativeImputer(estimator=RandomForestRegressor(**estimator_config),
+                                                                **c),
+    }
+
+    running_config = dict()
+    running_config.update(config)
+
+    running_estimator_config = dict()
+    running_estimator_config.update(estimator_config)
+
+    imputer = imputer_type_map[imputer_type](running_config, running_estimator_config)
+    train_df = random_missing_df.loc[random_missing_df.index.difference(missing_vals_idxs)]
+    imputer = imputer.fit(train_df)
+
+    imputed_mat = imputer.transform(random_missing_df)
+
+    imputed_df = pd.DataFrame(imputed_mat, columns=labeled_df.columns, index=labeled_df.index)
+    imputed_df = imputed_df.loc[missing_vals_idxs]
+
+    for col in labeled_df.columns:
+        imputed_df["{} (real)".format(col)] = labeled_df[col]
+        imputed_df["{} (imputed)".format(col)] = imputed_df[col]
+        imputed_df.drop([col], axis=1, inplace=True)
+
+    imputed_df["imputed"] = missing_col_per_pos
+
+    return imputed_df, imputer
